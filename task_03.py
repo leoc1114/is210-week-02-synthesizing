9f32a862422e91095649a063abebc8deb9293bd4
wei chen <wei.chen@spsmail.cuny.edu>
2015-02-07 00:33:50 +0000

    corrected task 3

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""A corrected python file."""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
